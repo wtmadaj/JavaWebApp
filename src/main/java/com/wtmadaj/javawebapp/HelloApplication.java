68733960a8ba59870b66fa8daa373643d0cc80af
Wes Madaj <wtmadaj@gmail.com>
2021-08-01 19:01:53 -0400

    initial commit

---
+++ src/main/java/com/wtmadaj/javawebapp/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.wtmadaj.javawebapp;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
